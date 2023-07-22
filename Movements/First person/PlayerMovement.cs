dfde783976cf65ed30fc8cd0cb368bdd574c7014
Hitsar <vlex09@ya.ru>
2023-07-22 17:43:10 +0300

    Make PuckUp with Joint

--- Movements/First person/PlayerMovement.cs
+++ Movements/First person/PlayerMovement.cs
@@ -12,27 +12,32 @@ namespace Player
         [SerializeField] private float _gravity = -9.81f;
         
         [SerializeField] private float _takeDistance = 5;
-        [SerializeField] private float _trowForce = 5;
+        [SerializeField] private float _trowForce = 6;
+        [SerializeField] private Joint _joint;
         [SerializeField] private LayerMask _canPickUpLayer;
 
         private PlayerInput _inputSystem;
         private CharacterController _characterController;
+        private Collider _playerCollider;
         private Camera _playerCamera;
 
         private Vector3 _velocity;
         private Vector2 _rotation;
 
-        private Rigidbody _currentObject;
+        private Rigidbody _currentRigidbodyObject;
+        private Collider _currentColliderObject;
 
         private void Start()
         {
             _inputSystem = new PlayerInput();
             _inputSystem.Player.Jump.performed += _ => Jump();
             _inputSystem.Player.PickUp.performed += _ => PickUp();
+            _inputSystem.Player.PickUp.canceled += _ => Drop();
             _inputSystem.Player.Trow.performed += _ => Drop(true);
             _inputSystem.Player.Enable();
 
             _characterController = GetComponent<CharacterController>();
+            _playerCollider = GetComponent<Collider>();
             _playerCamera = GetComponentInChildren<Camera>();
             
             Cursor.lockState = CursorLockMode.Locked;
@@ -60,39 +65,41 @@ namespace Player
             _velocity = new Vector3(move.x, _velocity.y, move.z);
             
             if (_characterController.isGrounded) _velocity.y = -0.1f;
-            
             else _velocity.y += _gravity * Time.fixedDeltaTime;
-            
-            if (_inputSystem.Player.PickUp.WasReleasedThisFrame()) Drop(); 
         }
 
         private void Jump() { if (_characterController.isGrounded) _velocity = Vector3.up * _jumpForce; }
 
         private void PickUp()
         {
-            if (Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out RaycastHit hit, _takeDistance, _canPickUpLayer))
-            {
-                _currentObject = hit.collider.gameObject.GetComponent<Rigidbody>();
+            if (!Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out RaycastHit hit, _takeDistance, _canPickUpLayer)) return;
+            
+            _currentRigidbodyObject = hit.collider.gameObject.GetComponent<Rigidbody>();
+            _currentColliderObject = _currentRigidbodyObject.GetComponent<Collider>();
 
-                float distance = Vector3.Distance(_playerCamera.transform.position, hit.collider.gameObject.transform.position);
-                
-                _currentObject.gameObject.transform.SetParent(_playerCamera.transform, false);
-                _currentObject.gameObject.transform.localPosition = default;
-                _currentObject.gameObject.transform.rotation = default;
-                _currentObject.gameObject.transform.localPosition += Vector3.forward * distance;
-                
-                _currentObject.isKinematic = true;
-            }
+            float distance = Vector3.Distance(_playerCamera.transform.position, hit.collider.gameObject.transform.position);
+            
+            Physics.IgnoreCollision(_playerCollider, _currentColliderObject);
+            _currentRigidbodyObject.drag = 10;
+            
+            _joint.gameObject.transform.localPosition = default;
+            _joint.gameObject.transform.localPosition += Vector3.forward * distance;
+            _joint.connectedBody = _currentRigidbodyObject;
         }
 
         private void Drop(bool isTrow = false)
         {
-            if (_currentObject == null) return;
+            if (_currentRigidbodyObject == null) return;
             
-            _currentObject.transform.parent = null;
-            _currentObject.isKinematic = false;
-            if (isTrow) _currentObject.AddForce(_playerCamera.transform.forward * _trowForce, ForceMode.Impulse);
-            _currentObject = null;
+            _joint.connectedBody = null;
+            
+            _currentRigidbodyObject.velocity = Vector3.zero;
+            if (isTrow) _currentRigidbodyObject.AddForce(_playerCamera.transform.forward * _trowForce, ForceMode.Impulse);
+            _currentRigidbodyObject.drag = 0;
+            
+            Physics.IgnoreCollision(_playerCollider, _currentColliderObject, false);
+            
+            _currentRigidbodyObject = null;
         }
     }
 }
