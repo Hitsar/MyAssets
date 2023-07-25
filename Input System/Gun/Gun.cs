acd653e818f328dd1dbd0a0ab2dccb0aa183c2f3
Hitsar <vlex09@ya.ru>
2023-07-25 13:26:32 +0300

    Add Input map, namespace

--- Input System/Gun/Gun.cs
+++ Input System/Gun/Gun.cs
@@ -2,53 +2,56 @@ using System.Collections.Generic;
 using Pool;
 using UnityEngine;
 
-public class Gun : MonoBehaviour
+namespace Gun
 {
-    [SerializeField] private Bullet _bullet;
-    [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private float _recharge = 0.5f;
-    [Header("Pool")]
-    [SerializeField] private GameObject _container;
-    [SerializeField] private int _quantity = 1;
-    
-    private readonly List<Bullet> _pool = new List<Bullet>();
-    
-    private SpriteRenderer _spriteRenderer;
-    private InputSystem _inputSystem;
-    private ObjectPool _objectPool;
-    
-    private Camera _main;
-    private float _time;
-
-    private void Start()
+    public class Gun : MonoBehaviour
     {
-        _main = Camera.main;
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _objectPool = new ObjectPool(_bullet.gameObject, _container, _quantity);
+        [SerializeField] private Bullet _bullet;
+        [SerializeField] private Transform _spawnPoint;
+        [SerializeField] private float _recharge = 0.5f;
+        [Header("Pool")]
+        [SerializeField] private GameObject _container;
+        [SerializeField] private int _quantity = 1;
+    
+        private readonly List<Bullet> _pool = new List<Bullet>();
+    
+        private SpriteRenderer _spriteRenderer;
+        private InputSystem _inputSystem;
+        private ObjectPool _objectPool;
+    
+        private Camera _main;
+        private float _time;
 
-        _inputSystem = new InputSystem();
-        _inputSystem.Gun.Shoot.performed += _ => Shoot();
-        _inputSystem.Gun.Enable();
-    }
-
-    private void FixedUpdate()
-    {
-        Vector3 difference = _main.ScreenToWorldPoint(_inputSystem.Gun.ShootDirection.ReadValue<Vector2>()) - transform.position;
-        float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rotateZ);
-        
-        _time -= Time.fixedDeltaTime;
-        _spriteRenderer.flipY = rotateZ >= 90 || rotateZ <= -90;
-    }
-
-    protected virtual void Shoot()
-    {
-        if (_time <= 0 && _objectPool.TryGetObject(out GameObject bullet))
+        private void Start()
         {
-            bullet.gameObject.transform.position = _spawnPoint.position;
-            bullet.gameObject.transform.rotation = _spawnPoint.rotation;
-            bullet.gameObject.transform.gameObject.SetActive(true);
-            _time = _recharge;
+            _main = Camera.main;
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _objectPool = new ObjectPool(_bullet.gameObject, _container, _quantity);
+
+            _inputSystem = new InputSystem();
+            _inputSystem.Gun.Shoot.performed += _ => Shoot();
+            _inputSystem.Gun.Enable();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector3 difference = _main.ScreenToWorldPoint(_inputSystem.Gun.ShootDirection.ReadValue<Vector2>()) - transform.position;
+            float rotateZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, rotateZ);
+        
+            _time -= Time.fixedDeltaTime;
+            _spriteRenderer.flipY = rotateZ >= 90 || rotateZ <= -90;
+        }
+
+        protected virtual void Shoot()
+        {
+            if (_time <= 0 && _objectPool.TryGetObject(out GameObject bullet))
+            {
+                bullet.gameObject.transform.position = _spawnPoint.position;
+                bullet.gameObject.transform.rotation = _spawnPoint.rotation;
+                bullet.gameObject.transform.gameObject.SetActive(true);
+                _time = _recharge;
+            }
         }
     }
 }
